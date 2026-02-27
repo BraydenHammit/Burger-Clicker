135ac33b7520cb255f8d6a18be268442c1b583f2
Sheldon1436 <bh547516@georgetownisd.org>
2026-02-27 13:37:53 -0600

    continued base layout

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import tkinter as tk
 
+clicks = 0
+upgrade = 1
+
 root = tk.Tk()
 root.geometry('700x700')
 root.title('Burger Clicker')
+
+def click():
+  global clicks
+  clicks += upgrade
+
+burger = tk.Button(root, image = tk.photoImage('path'), command = click)
+burger.grid(row=0,column=0)
 
 root.mainloop()
